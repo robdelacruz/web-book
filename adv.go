f0a050f9eefeaa31f6827c8e7649a0d129cffe8b
rob <robdelacruz@gmail.com>
2020-03-26 10:55:32 +0800

    add login, logout, createaccount, createpage

--- adv.go
+++ adv.go
@@ -1,6 +1,707 @@
 package main
 
-import ()
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const ADMIN_ID = 1
+
+type User struct {
+	Userid   int64
+	Username string
+	Active   bool
+	Email    string
+}
+
+type Site struct {
+	Title string
+	Desc  string
+}
+
+type Page struct {
+	Pageid int64
+	Title  string
+	Body   string
+}
 
 func main() {
+	os.Args = os.Args[1:]
+	sw, parms := parseArgs(os.Args)
+
+	// [-i new_file]  Create and initialize book file
+	if sw["i"] != "" {
+		dbfile := sw["i"]
+		if fileExists(dbfile) {
+			s := fmt.Sprintf("File '%s' already exists. Can't initialize it.\n", dbfile)
+			fmt.Printf(s)
+			os.Exit(1)
+		}
+		createAndInitTables(dbfile)
+		os.Exit(0)
+	}
+
+	// Need to specify a db file as first parameter.
+	if len(parms) == 0 {
+		s := `Usage:
+
+Start webservice using existing book file:
+	adv <book_file>
+
+Initialize new book file:
+	nb -i <book_file>
+
+`
+		fmt.Printf(s)
+		os.Exit(0)
+	}
+
+	// Exit if specified notes file doesn't exist.
+	dbfile := parms[0]
+	if !fileExists(dbfile) {
+		s := fmt.Sprintf(`Book file '%s' doesn't exist. Create one using:
+	nb -i <book_file>
+`, dbfile)
+		fmt.Printf(s)
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("sqlite3", dbfile)
+	if err != nil {
+		fmt.Printf("Error opening '%s' (%s)\n", dbfile, err)
+		os.Exit(1)
+	}
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("/login/", loginHandler(db))
+	http.HandleFunc("/logout/", logoutHandler(db))
+	http.HandleFunc("/createaccount/", createaccountHandler(db))
+	http.HandleFunc("/", indexHandler(db))
+	http.HandleFunc("/createpage/", createpageHandler(db))
+	port := "8000"
+	fmt.Printf("Listening on %s...\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	log.Fatal(err)
+}
+
+func parseArgs(args []string) (map[string]string, []string) {
+	switches := map[string]string{}
+	parms := []string{}
+
+	standaloneSwitches := []string{}
+	definitionSwitches := []string{"i"}
+	fNoMoreSwitches := false
+	curKey := ""
+
+	for _, arg := range args {
+		if fNoMoreSwitches {
+			// any arg after "--" is a standalone parameter
+			parms = append(parms, arg)
+		} else if arg == "--" {
+			// "--" means no more switches to come
+			fNoMoreSwitches = true
+		} else if strings.HasPrefix(arg, "--") {
+			switches[arg[2:]] = "y"
+			curKey = ""
+		} else if strings.HasPrefix(arg, "-") {
+			if listContains(definitionSwitches, arg[1:]) {
+				// -a "val"
+				curKey = arg[1:]
+				continue
+			}
+			for _, ch := range arg[1:] {
+				// -a, -b, -ab
+				sch := string(ch)
+				if listContains(standaloneSwitches, sch) {
+					switches[sch] = "y"
+				}
+			}
+		} else if curKey != "" {
+			switches[curKey] = arg
+			curKey = ""
+		} else {
+			// standalone parameter
+			parms = append(parms, arg)
+		}
+	}
+
+	return switches, parms
+}
+
+func listContains(ss []string, v string) bool {
+	for _, s := range ss {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func fileExists(file string) bool {
+	_, err := os.Stat(file)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func idtoi(sid string) int64 {
+	if sid == "" {
+		return -1
+	}
+	n, err := strconv.Atoi(sid)
+	if err != nil {
+		return -1
+	}
+	return int64(n)
+}
+
+func atoi(s string) int {
+	if s == "" {
+		return -1
+	}
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return -1
+	}
+	return n
+}
+
+func atof(s string) float64 {
+	if s == "" {
+		return -1.0
+	}
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return -1.0
+	}
+	return f
+}
+
+func sqlstmt(db *sql.DB, s string) *sql.Stmt {
+	stmt, err := db.Prepare(s)
+	if err != nil {
+		log.Fatalf("db.Prepare() sql: '%s'\nerror: '%s'", s, err)
+	}
+	return stmt
+}
+
+func sqlexec(db *sql.DB, s string, pp ...interface{}) (sql.Result, error) {
+	stmt := sqlstmt(db, s)
+	defer stmt.Close()
+	return stmt.Exec(pp...)
+}
+
+func createAndInitTables(newfile string) {
+	if fileExists(newfile) {
+		s := fmt.Sprintf("File '%s' already exists. Can't initialize it.\n", newfile)
+		fmt.Printf(s)
+		os.Exit(1)
+	}
+
+	db, err := sql.Open("sqlite3", newfile)
+	if err != nil {
+		fmt.Printf("Error opening '%s' (%s)\n", newfile, err)
+		os.Exit(1)
+	}
+
+	ss := []string{
+		"BEGIN TRANSACTION;",
+		"CREATE TABLE page (page_id INTEGER PRIMARY KEY NOT NULL, title TEXT, body TEXT);",
+		"CREATE TABLE user (user_id INTEGER PRIMARY KEY NOT NULL, username TEXT, password TEXT, active INTEGER NOT NULL, email TEXT, CONSTRAINT unique_username UNIQUE (username));",
+		"INSERT INTO user (user_id, username, password, active, email) VALUES (1, 'admin', '', 1, '');",
+		"COMMIT;",
+	}
+
+	for _, s := range ss {
+		_, err := sqlexec(db, s)
+		if err != nil {
+			log.Printf("DB error setting up newsboard db on '%s' (%s)\n", newfile, err)
+			os.Exit(1)
+		}
+	}
+}
+
+func getLoginUser(r *http.Request, db *sql.DB) *User {
+	var u User
+	u.Userid = -1
+
+	c, err := r.Cookie("userid")
+	if err != nil {
+		return &u
+	}
+	userid := idtoi(c.Value)
+	if userid == -1 {
+		return &u
+	}
+	return queryUser(db, userid)
+}
+
+func queryUser(db *sql.DB, userid int64) *User {
+	var u User
+	u.Userid = -1
+
+	s := "SELECT user_id, username, active, email FROM user WHERE user_id = ?"
+	row := db.QueryRow(s, userid)
+	err := row.Scan(&u.Userid, &u.Username, &u.Active, &u.Email)
+	if err == sql.ErrNoRows {
+		return &u
+	}
+	if err != nil {
+		fmt.Printf("queryUser() db error (%s)\n", err)
+		return &u
+	}
+	return &u
+}
+
+func queryUsername(db *sql.DB, username string) *User {
+	var u User
+	u.Userid = -1
+
+	s := "SELECT user_id, username, active, email FROM user WHERE username = ?"
+	row := db.QueryRow(s, username)
+	err := row.Scan(&u.Userid, &u.Username, &u.Active, &u.Email)
+	if err == sql.ErrNoRows {
+		return &u
+	}
+	if err != nil {
+		fmt.Printf("queryUser() db error (%s)\n", err)
+		return &u
+	}
+	return &u
+}
+
+func querySite(db *sql.DB) *Site {
+	var site Site
+	s := "SELECT title, desc FROM site WHERE site_id = 1"
+	row := db.QueryRow(s)
+	err := row.Scan(&site.Title, &site.Desc)
+	if err == sql.ErrNoRows {
+		// Site settings row not defined yet, just use default Site values.
+		site.Title = "Game Book"
+		site.Desc = ""
+	} else if err != nil {
+		// DB error, log then use common site settings.
+		log.Printf("error reading site settings for siteid %d (%s)\n", 1, err)
+		site.Title = "Game Book"
+	}
+	return &site
+}
+
+func isCorrectPassword(inputPassword, hashedpwd string) bool {
+	if hashedpwd == "" && inputPassword == "" {
+		return true
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(hashedpwd), []byte(inputPassword))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func hashPassword(pwd string) string {
+	hashedpwd, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return string(hashedpwd)
+}
+
+func loginUser(w http.ResponseWriter, userid int64) {
+	suserid := fmt.Sprintf("%d", userid)
+	c := http.Cookie{
+		Name:  "userid",
+		Value: suserid,
+		Path:  "/",
+		// Expires: time.Now().Add(24 * time.Hour),
+	}
+	http.SetCookie(w, &c)
+}
+
+func unescapeUrl(qurl string) string {
+	returl := "/"
+	if qurl != "" {
+		returl, _ = url.QueryUnescape(qurl)
+	}
+	return returl
+}
+
+func handleDbErr(w http.ResponseWriter, err error, sfunc string) bool {
+	if err == sql.ErrNoRows {
+		http.Error(w, "Not found.", 404)
+		return true
+	}
+	if err != nil {
+		log.Printf("%s: database error (%s)\n", sfunc, err)
+		http.Error(w, "Server database error.", 500)
+		return true
+	}
+	return false
+}
+
+func validateLogin(w http.ResponseWriter, login *User) bool {
+	if login.Userid == -1 {
+		http.Error(w, "Not logged in.", 401)
+		return false
+	}
+	if !login.Active {
+		http.Error(w, "Not an active user.", 401)
+		return false
+	}
+	return true
+}
+
+func printPageHead(w io.Writer, jsurls []string, cssurls []string) {
+	fmt.Fprintf(w, "<!DOCTYPE html>\n")
+	fmt.Fprintf(w, "<html>\n")
+	fmt.Fprintf(w, "<head>\n")
+	fmt.Fprintf(w, "<meta charset=\"utf-8\">\n")
+	fmt.Fprintf(w, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n")
+	fmt.Fprintf(w, "<title>Website</title>\n")
+	fmt.Fprintf(w, "<link rel=\"stylesheet\" type=\"text/css\" href=\"/static/style.css\">\n")
+	fmt.Fprintf(w, "<link rel=\"stylesheet\" type=\"text/css\" href=\"/static/nbstyle.css\">\n")
+	for _, cssurl := range cssurls {
+		fmt.Fprintf(w, "<link rel=\"stylesheet\" type=\"text/css\" href=\"%s\">\n", cssurl)
+	}
+	for _, jsurl := range jsurls {
+		fmt.Fprintf(w, "<script src=\"%s\" defer></script>\n", jsurl)
+	}
+	fmt.Fprintf(w, "</head>\n")
+	fmt.Fprintf(w, "<body>\n")
+	fmt.Fprintf(w, "<section class=\"body\">\n")
+}
+
+func printPageFoot(w io.Writer) {
+	fmt.Fprintf(w, "</section>\n")
+	fmt.Fprintf(w, "</body>\n")
+	fmt.Fprintf(w, "</html>\n")
+}
+
+func printPageNav(w http.ResponseWriter, login *User) {
+	fmt.Fprintf(w, "<header class=\"masthead mb-sm\">\n")
+	fmt.Fprintf(w, "<nav class=\"navbar\">\n")
+
+	// Menu section (left part)
+	fmt.Fprintf(w, "<div>\n")
+	var title string
+	if title == "" {
+		title = "Game Book"
+	}
+	fmt.Fprintf(w, "<h1 class=\"heading\"><a href=\"/\">%s</a></h1>\n", title)
+	fmt.Fprintf(w, "<ul class=\"line-menu\">\n")
+	fmt.Fprintf(w, "  <li><a href=\"/?latest=1\">latest</a></li>\n")
+	if login.Userid != -1 && login.Active {
+		fmt.Fprintf(w, "  <li><a href=\"/submit/\">submit</a></li>\n")
+	}
+	fmt.Fprintf(w, "</ul>\n")
+	fmt.Fprintf(w, "</div>\n")
+
+	// User section (right part)
+	fmt.Fprintf(w, "<ul class=\"line-menu right\">\n")
+	if login.Userid == -1 {
+		fmt.Fprintf(w, "<li><a href=\"/login\">login</a></li>\n")
+	} else if login.Userid == ADMIN_ID {
+		fmt.Fprintf(w, "<li><a href=\"/adminsetup/\">%s</a></li>\n", login.Username)
+		fmt.Fprintf(w, "<li><a href=\"/logout\">logout</a></li>\n")
+	} else {
+		fmt.Fprintf(w, "<li><a href=\"/usersetup/\">%s</a></li>\n", login.Username)
+		fmt.Fprintf(w, "<li><a href=\"/logout\">logout</a></li>\n")
+	}
+	fmt.Fprintf(w, "</ul>\n")
+
+	fmt.Fprintf(w, "</nav>\n")
+	fmt.Fprintf(w, "</header>\n")
+}
+
+func loginHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var errmsg string
+		var f struct{ username, password string }
+
+		login := getLoginUser(r, db)
+		qfrom := r.FormValue("from")
+
+		if r.Method == "POST" {
+			f.username = r.FormValue("username")
+			f.password = r.FormValue("password")
+
+			s := "SELECT user_id, password, active FROM user WHERE username = ?"
+			row := db.QueryRow(s, f.username, f.password)
+
+			var userid int64
+			var hashedpwd string
+			var active int
+			err := row.Scan(&userid, &hashedpwd, &active)
+
+			for {
+				if err == sql.ErrNoRows {
+					errmsg = "Incorrect username or password"
+					break
+				}
+				if err != nil {
+					errmsg = "A problem occured. Please try again."
+					break
+				}
+				if !isCorrectPassword(f.password, hashedpwd) {
+					errmsg = "Incorrect username or password"
+					break
+				}
+				if active == 0 {
+					errmsg = fmt.Sprintf("User '%s' is inactive.", f.username)
+					break
+				}
+
+				loginUser(w, userid)
+
+				http.Redirect(w, r, unescapeUrl(qfrom), http.StatusSeeOther)
+				return
+			}
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		printPageHead(w, nil, nil)
+		printPageNav(w, login)
+
+		fmt.Fprintf(w, "<section class=\"main\">\n")
+		fmt.Fprintf(w, "<form class=\"simpleform\" action=\"/login/?from=%s\" method=\"post\">\n", qfrom)
+		fmt.Fprintf(w, "<h1 class=\"heading\">Login</h1>")
+		if errmsg != "" {
+			fmt.Fprintf(w, "<div class=\"control\">\n")
+			fmt.Fprintf(w, "<p class=\"error\">%s</p>\n", errmsg)
+			fmt.Fprintf(w, "</div>\n")
+		}
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<label for=\"username\">username</label>\n")
+		fmt.Fprintf(w, "<input id=\"username\" name=\"username\" type=\"text\" size=\"20\" value=\"%s\">\n", f.username)
+		fmt.Fprintf(w, "</div>\n")
+
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<label for=\"password\">password</label>\n")
+		fmt.Fprintf(w, "<input id=\"password\" name=\"password\" type=\"password\" size=\"20\" value=\"%s\">\n", f.password)
+		fmt.Fprintf(w, "</div>\n")
+
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<button class=\"submit\">login</button>\n")
+		fmt.Fprintf(w, "</div>\n")
+		fmt.Fprintf(w, "</form>\n")
+
+		fmt.Fprintf(w, "<p class=\"mt-xl\"><a href=\"/createaccount/?from=%s\">Create New Account</a></p>\n", qfrom)
+		fmt.Fprintf(w, "</section>\n")
+
+		printPageFoot(w)
+	}
+}
+
+func logoutHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := http.Cookie{
+			Name:   "userid",
+			Value:  "",
+			Path:   "/",
+			MaxAge: 0,
+		}
+		http.SetCookie(w, &c)
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func isUsernameExists(db *sql.DB, username string) bool {
+	s := "SELECT user_id FROM user WHERE username = ?"
+	row := db.QueryRow(s, username)
+	var userid int64
+	err := row.Scan(&userid)
+	if err == sql.ErrNoRows {
+		return false
+	}
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func createaccountHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var errmsg string
+		var f struct{ username, email, password, password2 string }
+
+		login := getLoginUser(r, db)
+		qfrom := r.FormValue("from")
+
+		if r.Method == "POST" {
+			f.username = r.FormValue("username")
+			f.email = r.FormValue("email")
+			f.password = r.FormValue("password")
+			f.password2 = r.FormValue("password2")
+			for {
+				if f.password != f.password2 {
+					errmsg = "re-entered password doesn't match"
+					f.password = ""
+					f.password2 = ""
+					break
+				}
+				if isUsernameExists(db, f.username) {
+					errmsg = fmt.Sprintf("username '%s' already exists", f.username)
+					break
+				}
+
+				hashedPassword := hashPassword(f.password)
+				s := "INSERT INTO user (username, password, active, email) VALUES (?, ?, ?, ?);"
+				result, err := sqlexec(db, s, f.username, hashedPassword, 1, f.email)
+				if err != nil {
+					log.Printf("DB error creating user: %s\n", err)
+					errmsg = "A problem occured. Please try again."
+					break
+				}
+				newid, err := result.LastInsertId()
+				if err == nil {
+					loginUser(w, newid)
+				} else {
+					// DB doesn't support getting newly added userid, so login manually.
+					qfrom = "/login/"
+				}
+
+				http.Redirect(w, r, unescapeUrl(qfrom), http.StatusSeeOther)
+				return
+			}
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		printPageHead(w, nil, nil)
+		printPageNav(w, login)
+
+		fmt.Fprintf(w, "<section class=\"main\">\n")
+		fmt.Fprintf(w, "<form class=\"simpleform\" action=\"/createaccount/?from=%s\" method=\"post\">\n", qfrom)
+		fmt.Fprintf(w, "<h1 class=\"heading\">Create Account</h1>")
+		if errmsg != "" {
+			fmt.Fprintf(w, "<div class=\"control\">\n")
+			fmt.Fprintf(w, "<p class=\"error\">%s</p>\n", errmsg)
+			fmt.Fprintf(w, "</div>\n")
+		}
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<label for=\"username\">username</label>\n")
+		fmt.Fprintf(w, "<input id=\"username\" name=\"username\" type=\"text\" size=\"20\" value=\"%s\">\n", f.username)
+		fmt.Fprintf(w, "</div>\n")
+
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<label for=\"email\">email</label>\n")
+		fmt.Fprintf(w, "<input id=\"email\" name=\"email\" type=\"email\" size=\"20\" value=\"%s\">\n", f.email)
+		fmt.Fprintf(w, "</div>\n")
+
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<label for=\"password\">password</label>\n")
+		fmt.Fprintf(w, "<input id=\"password\" name=\"password\" type=\"password\" size=\"20\" value=\"%s\">\n", f.password)
+		fmt.Fprintf(w, "</div>\n")
+
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<label for=\"password2\">re-enter password</label>\n")
+		fmt.Fprintf(w, "<input id=\"password2\" name=\"password2\" type=\"password\" size=\"20\" value=\"%s\">\n", f.password2)
+		fmt.Fprintf(w, "</div>\n")
+
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<button class=\"submit\">create account</button>\n")
+		fmt.Fprintf(w, "</div>\n")
+		fmt.Fprintf(w, "</form>\n")
+		fmt.Fprintf(w, "</section>\n")
+
+		printPageFoot(w)
+	}
+}
+
+func indexHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		login := getLoginUser(r, db)
+
+		w.Header().Set("Content-Type", "text/html")
+		printPageHead(w, nil, nil)
+		printPageNav(w, login)
+
+		fmt.Fprintf(w, "<section class=\"main\">\n")
+
+		fmt.Fprintf(w, "</section>\n")
+		printPageFoot(w)
+	}
+}
+
+func createPageUrl(id int64) string {
+	return fmt.Sprintf("/page/?id=%d", id)
+}
+
+func createpageHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		login := getLoginUser(r, db)
+
+		var p Page
+
+		var errmsg string
+		if r.Method == "POST" {
+			if !validateLogin(w, login) {
+				return
+			}
+
+			for {
+				p.Title = strings.TrimSpace(r.FormValue("title"))
+				p.Body = strings.TrimSpace(r.FormValue("body"))
+				if p.Body == "" {
+					errmsg = "Please enter some text."
+					break
+				}
+
+				s := "INSERT INTO page (title, body) VALUES (?, ?)"
+				result, err := sqlexec(db, s, p.Title, p.Body)
+				if err != nil {
+					log.Printf("DB error creating submission (%s)\n", err)
+					errmsg = "A problem occured. Please try again."
+					break
+				}
+				newid, err := result.LastInsertId()
+				if err != nil {
+					http.Redirect(w, r, "/", http.StatusSeeOther)
+					return
+				}
+				http.Redirect(w, r, createPageUrl(newid), http.StatusSeeOther)
+				return
+			}
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		printPageHead(w, nil, nil)
+		printPageNav(w, login)
+		fmt.Fprintf(w, "<section class=\"main\">\n")
+
+		fmt.Fprintf(w, "<form class=\"simpleform mb-2xl\" method=\"post\" action=\"/createpage/\">\n")
+		if errmsg != "" {
+			fmt.Fprintf(w, "<div class=\"control\">\n")
+			fmt.Fprintf(w, "<p class=\"error\">%s</p>\n", errmsg)
+			fmt.Fprintf(w, "</div>\n")
+		}
+
+		fmt.Fprintf(w, "<div class=\"control\">\n")
+		fmt.Fprintf(w, "<label for=\"title\">title</label>\n")
+		fmt.Fprintf(w, "<input id=\"title\" name=\"title\" type=\"text\" size=\"60\" value=\"%s\">\n", p.Title)
+		fmt.Fprintf(w, "</div>\n")
+
+		fmt.Fprintf(w, "  <div class=\"control\">\n")
+		fmt.Fprintf(w, "    <label for=\"body\">text</label>\n")
+		fmt.Fprintf(w, "    <textarea id=\"body\" name=\"body\" rows=\"6\" cols=\"60\">%s</textarea>\n", p.Body)
+		fmt.Fprintf(w, "  </div>\n")
+
+		fmt.Fprintf(w, "  <div class=\"control\">\n")
+		fmt.Fprintf(w, "    <button class=\"submit\">submit</button>\n")
+		fmt.Fprintf(w, "  </div>\n")
+		fmt.Fprintf(w, "</form>\n")
+
+		fmt.Fprintf(w, "</section>\n")
+		printPageFoot(w)
+	}
 }
